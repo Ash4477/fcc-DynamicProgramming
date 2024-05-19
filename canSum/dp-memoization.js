7b65c9f913ba8501d0c09b61bc996c3ddae8fa31
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2024-05-19 12:31:04 +0500

    dp-memoization implementation

---
+++ canSum/dp-memoization.js
@@ -0,0 +1,25 @@
+const canSum = (targetSum, numbers, memo = {}) => {
+    if (targetSum in memo) return memo[targetSum];
+    if (targetSum === 0) return true;
+    if (targetSum < 0) return false;
+
+    for (let num of numbers) {
+        const remainder = targetSum - num;
+        if (canSum(remainder, numbers, memo) === true) {
+            memo[targetSum] = true;
+            return true;
+        }
+    }
+    memo[targetSum] = false;
+    return false;
+};
+
+console.log(canSum(7, [2,3]));
+console.log(canSum(7, [5,3,4,7]));
+console.log(canSum(7, [2,4]));
+console.log(canSum(8, [2,3,5]));
+console.log(canSum(300, [7,14]));
+
+// let m = targetSum, n = arraylength
+// Time Complexity: O(n*m)
+// Space Complexity: O(m)
