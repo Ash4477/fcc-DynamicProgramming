da08ca32a67cfc2a98d74d85c30fc51600618d00
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2024-05-25 20:05:01 +0500

    finish course

--- allConstruct/tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-console.log(allConstruct("abcdef",["ab", "abc", "cd", "def", "abcd", "ef", "c"]));
-// console.log(allConstruct("skateboard",["bo", "rd", "ate", "t", "ska", "sk", "boar"]));
-// console.log(allConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",["e","ee","eee","eeee","eeeee"])); // takes long time
